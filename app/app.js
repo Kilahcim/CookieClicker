0688f14f2ca79d88115625c01f4a9e781d7a6b59
Krzysztof Michalik <michalik.krzysztof@poczta.pl>
2017-12-09 12:31:25 +0100

    main functionality

--- app/app.js
+++ app/app.js
@@ -6,15 +6,17 @@ $(function(){
   amount.text(cookieCounter);
   var productivity = $('.productivity') ;
   var productPerSecond = 0;
-  var cookieCounter = amount.text();
+  var cookieCounter = 0;
+
+
+  mainCookie.on('click', function(){
+    cookieCouonter = 1 + parseInt(cookieCounter,10);
+    console.log(cookieCounter);
+    amount.text(cookieCounter);
+  })
   setInterval(function(){
       amount.text(cookieCounter);
   },1000);
-
-  mainCookie.on('click', function(){
-    cookieCounter++;
-    amount.text(cookieCounter);
-  })
 
   // Function for button click:
   button.on('click', function(){
@@ -40,9 +42,14 @@ $(function(){
       $(this).attr('data-price', newPrice);
       var cost = $(this).parent().prev('.cost');
       cost.text($(this).attr('data-price'));
+      productPerSecond += parseInt($(this).attr('data-productivity'));
+      productivity.text(productPerSecond);
+      var cookiesPS = parseInt($(this).attr('data-productivity'));
+
       setInterval(function(){
-        cookieCounter += 8;
+        cookieCounter += cookiesPS ;
       },1000)
+
 
     }
     if(parseInt(amount.text()) >= $(this).attr('data-price') && ($(this).attr('data-name') === 'farm')) {
@@ -52,20 +59,28 @@ $(function(){
       $(this).attr('data-price', newPrice);
       var cost = $(this).parent().prev('.cost');
       cost.text($(this).attr('data-price'));
+      productPerSecond += parseInt($(this).attr('data-productivity'));
+      productivity.text(productPerSecond);
+      var cookiesPS = parseInt($(this).attr('data-productivity'));
+
       setInterval(function(){
-        cookieCounter += 64;
+        cookieCounter += cookiesPS ;
       },1000)
 
     }
     if(parseInt(amount.text()) >= $(this).attr('data-price') && ($(this).attr('data-name') === 'bakery')) {
       cookieCounter = cookieCounter - $(this).attr('data-price');
       amount.text((amount.text() - $(this).attr('data-price')).toFixed(2));
-      var newPrice = ($(this).attr('data-price') * 1 + 2).toFixed(2);
+      var newPrice = ($(this).attr('data-price') * 1.2 + 2).toFixed(2);
       $(this).attr('data-price', newPrice);
       var cost = $(this).parent().prev('.cost');
       cost.text($(this).attr('data-price'));
+      productPerSecond += parseInt($(this).attr('data-productivity'));
+      productivity.text(productPerSecond);
+      var cookiesPS = parseInt($(this).attr('data-productivity'));
+
       setInterval(function(){
-        cookieCounter += 512;
+        cookieCounter += cookiesPS ;
       },1000)
 
     }
@@ -76,8 +91,12 @@ $(function(){
       $(this).attr('data-price', newPrice);
       var cost = $(this).parent().prev('.cost');
       cost.text($(this).attr('data-price'));
+      productPerSecond += parseInt($(this).attr('data-productivity'));
+      productivity.text(productPerSecond);
+      var cookiesPS = parseInt($(this).attr('data-productivity'));
+
       setInterval(function(){
-        cookieCounter += 4096;
+        cookieCounter += cookiesPS ;
       },1000)
 
     }
